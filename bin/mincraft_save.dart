780effa5f2318ca0aa173fa5b3b242e562d8b773
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-11-27 19:31:24 -0800

    initial commit

---
+++ bin/mincraft_save.dart
@@ -0,0 +1,7 @@
+import 'package:mincraft_save/mincraft_save.dart' as mincraft_save;
+
+// yes | cp -r /home/yuki/.minecraft/saves/test /media/yuki/INTENSO
+// yes | cp -r /media/yuki/INTENSO/test /home/yuki/.minecraft/saves
+void main(List<String> arguments) {
+  print('Hello world');
+}
---
+++ test/mincraft_save_test.dart
@@ -0,0 +1,8 @@
+import 'package:mincraft_save/mincraft_save.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
